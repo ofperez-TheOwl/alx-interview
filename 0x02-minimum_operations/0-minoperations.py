d46ff1274c027bc6fa88a562328320e4234d1f55
ofperez-TheOwl <odidiperez@gmail.com>
2023-01-10 20:44:05 +0000

    Minimum operations completed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+0-minoperations
+"""
+
+
+def minOperations(n: int) -> int:
+    """Calculate the min of operations to obtain n"""
+    # base cases
+    if (n == 1):
+        return (0)
+    if (n == 2):
+        return (2)
+    if (n == 3):
+        return (3)
+
+    # recursive instructions
+    if (n % 2 == 0):
+        res = minOperations(n / 2)
+        if (res == 0):
+            return (0)
+        else:
+            return (res + 2)
+    else:
+        if (n % 3 == 0):
+            res = minOperations(n / 3)
+            if (res == 0):
+                return (0)
+            else:
+                return (res + 3)
+        else:
+            return (0)
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,11 @@
+# **Pascal's Triangle**
+
+## **Description**
+
+In a text file, there is a single character `H`. Your text editor can execute only two operations in this file: `Copy All` and `Paste`.
+Given a number `n`, write a method that calculates the fewest number of operations needed to result in exactly `n` `H` characters in the file.
+
+I will use a recursive function to calculate `n`.
+
+####	**Author**
+***Perez OF***
