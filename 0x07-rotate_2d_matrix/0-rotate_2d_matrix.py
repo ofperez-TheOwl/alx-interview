49267a152674549e0b0c944af0cb0d163aed2159
ofperez-TheOwl <odidiperez@gmail.com>
2024-01-28 21:52:03 +0000

    Rotate 2D matrix second  draft

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,21 +2,14 @@
 
 def rotate_2d_matrix(matrix):
     n = len(matrix)
-    v_matrix = []
-    i = 0
 
-    for line in matrix:
-        v_line = []
-        for element in line:
-            v_line.append(element)
-        v_matrix.append(v_line)
-
-    while (i < n):
-        j = n - 1
-        k = 0
-        while (j > -1):
-            matrix[i][k] = v_matrix[j][i]
-            j = j - 1
-            k = k + 1
-        i = i + 1
-
+    #rotattion can be assimilate to the composition of a transposition and reversion
+    
+    #transposition
+    for i in range(n):
+        for j in range (i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # reversion
+    for i in range(n):
+        matrix[i].reverse()
