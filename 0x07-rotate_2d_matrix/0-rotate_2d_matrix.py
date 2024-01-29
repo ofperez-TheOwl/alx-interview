9699a96007ee92823d52b0ff0b5e6a8f54308b42
ofperez-TheOwl <odidiperez@gmail.com>
2024-01-29 01:55:21 +0000

    Rotate 2D matrix: adding pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,16 +4,21 @@ rotate 2D Matrix
 It rotates a suare matrix by 90 degre clockwise
 TheOwl
 """
+
+
 def rotate_2d_matrix(matrix):
+    # matrix is a non empty square matrix
+
     n = len(matrix)
 
-    #rotattion can be assimilate to the composition of a transposition and reversion
-    
-    #transposition
+    # rotattion can be assimilate to the
+    # composition of a transposition and reversion
+
+    # transposition
     for i in range(n):
-        for j in range (i + 1, n):
+        for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # reversion
     for i in range(n):
         matrix[i].reverse()
