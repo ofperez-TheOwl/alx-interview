988732ac1698800ac94e2a443f3d873c89413ced
ofperez-TheOwl <odidiperez@gmail.com>
2024-01-29 14:21:18 +0000

    Make change: setting directory and files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+makeChange
+Determine the minimal number of coins needed to meet
+a given amount and a certains set of coins
+"""
+
+
+def makeChange(coins, total):
+    # coins : a list of available coins
+    # total : amount to meet
+    return n
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,8 @@
+# **Make change**
+
+## **Description**
+
+The objective is to calculate the fewest number of coins needed to meet a given amount with any list of available coins.
+
+####	**Author**
+***Perez OF***
