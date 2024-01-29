0aceaee7176158489959dfba9a7a313c1016fdf2
ofperez-TheOwl <odidiperez@gmail.com>
2024-01-29 14:47:25 +0000

    Make change: first draft

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,4 +9,19 @@ a given amount and a certains set of coins
 def makeChange(coins, total):
     # coins : a list of available coins
     # total : amount to meet
-    return n
+
+    if (total <= 0):
+        return (0)
+    n = 0
+
+    # sort the coins
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        n = n + total // coin
+        rest = total % coin
+        if (rest == 0):
+            return (n)
+        total = rest
+
+    return (-1)
